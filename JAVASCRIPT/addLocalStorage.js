3bd21ec078e9b88b4aa471bf19a115183c182fbe
laKostin951 <58942624+laKostin951@users.noreply.github.com>
2020-02-04 18:07:20 +0100

    Create addLocalStorage.js

---
+++ JAVASCRIPT/addLocalStorage.js
@@ -0,0 +1,450 @@
+localStorage.setItem("poster_movies", JSON.stringify([{
+        "poster": "FILES/POSTER/the-lion-king-2019-movie.jpg",
+        "title": "Lion King",
+        "id": 5
+    }, {
+        "poster": "FILES/POSTER/avengers-logo-purple-minimal.png",
+        "title": "Avengers Endgame",
+        "id": 6
+    }, {
+        "poster": "FILES/POSTER/dora-and-the-lost-city-of-gold-2019-movie.jpg",
+        "title": "Dora and the Lost City of Gold",
+        "id": 7
+    }, {
+        "poster": "FILES/POSTER/fast-furious-presents-hobbs-shaw-dwayne-johnson-jason.jpg",
+        "title": "Fast & Furious Present: Hobbs & Shaw",
+        "id": 8
+    }, {
+        "poster": "FILES/POSTER/joker-2019-movie.jpg",
+        "title": "Joker",
+        "id": 9
+    }, {
+        "poster": "FILES/POSTER/jumanji-the-next-level-kevin-hart-dwayne-johnson.jpg",
+        "title": "Jumanji:The Next Level",
+        "id": 10
+    }, {
+        "poster": "FILES/POSTER/movie-poster-of-john-wick-3.jpg",
+        "title": "John Wick: Chapter 3 - Parabellum",
+        "id": 11
+    }, {
+        "poster": "FILES/POSTER/movie-the-dark-knight-rises.jpg",
+        "title": "The Dark Knight Rises",
+        "id": 12
+    }, {
+        "poster": "FILES/POSTER/red-balloons-it-chapter-2-movie.jpg",
+        "title": "It Chapter Two",
+        "id": 13
+    }]
+
+
+));
+
+localStorage.setItem("All_movies", JSON.stringify([{
+        "title": "Knivs Out",
+        "director": "Rian Johnson",
+        "genre": "Drama",
+        "year": "2019",
+        "price": "2.99",
+        "quantity": 99,
+        "cover": "FILES/COVER/knivs_out.jpg",
+        "poster": null,
+        "descrition": ""
+    }, {
+        "title": "The Irishamn",
+        "director": "Martin Scorsese",
+        "genre": "Crime",
+        "year": "2019",
+        "price": "2.99",
+        "quantity": 0,
+        "cover": "FILES/COVER/irishman.jpg",
+        "poster": null,
+        "descrition": ""
+    }, {
+        "title": "Black Panther",
+        "director": "Rian Johnson",
+        "genre": "Fantasy",
+        "year": "2018",
+        "price": "1.99",
+        "quantity": 81,
+        "cover": "FILES/COVER/black-panther3.jpg",
+        "poster": null,
+        "descrition": ""
+    }, {
+        "title": "A Star Is Born",
+        "director": "Martin Scorsese",
+        "genre": "Romance",
+        "year": "2018",
+        "price": "3.99",
+        "quantity": "70",
+        "cover": "FILES/COVER/a star is born.jpg",
+        "poster": null,
+        "descrition": ""
+    }, {
+        "title": "Aquaman",
+        "director": "James Wan",
+        "genre": "Sci-Fi",
+        "year": "2018",
+        "price": "0.99",
+        "quantity": 80,
+        "cover": "FILES/COVER/6319745_so.jpg",
+        "poster": null,
+        "descrition": ""
+    }, {
+        "title": "Lion King2",
+        "director": "Jon Faverea",
+        "genre": "Drama",
+        "year": "2019",
+        "price": "2.99",
+        "quantity": "88",
+        "cover": "FILES/COVER/lion_king_ver22.jpg",
+        "poster": false,
+        "descrition": ""
+    }, {
+        "title": "Avengers Endgame",
+        "director": "Anthony Russo",
+        "genre": "Fantasy",
+        "year": "2019",
+        "price": "4.99",
+        "quantity": 95,
+        "cover": "FILES/COVER/81%2BNup8-8NL._SL1500_.jpg",
+        "poster": "FILES/POSTER/avengers-logo-purple-minimal.png",
+        "descrition": ""
+    }, {
+        "title": "Dora and the Lost City of Gold",
+        "director": "James Bobin",
+        "genre": "Adventure",
+        "year": "2019",
+        "price": "0.99",
+        "quantity": 93,
+        "cover": "FILES/COVER/MV5BOTVhMzYxNjgtYzYwOC00MGIwLWJmZGEtMjgwMzgxMWUwNmRhXkEyXkFqcGdeQXVyNjg2NjQwMDQ%40._V1_.jpg",
+        "poster": "FILES/POSTER/dora-and-the-lost-city-of-gold-2019-movie.jpg",
+        "descrition": ""
+    }, {
+        "title": "Fast & Furious Present: Hobbs & Shaw",
+        "director": "David Leitch",
+        "genre": "Action",
+        "year": "2019",
+        "price": "1.99",
+        "quantity": 94,
+        "cover": "FILES/COVER/2cb0b40827c1d8b53df0d68013674709.jpg",
+        "poster": "FILES/POSTER/fast-furious-presents-hobbs-shaw-dwayne-johnson-jason.jpg",
+        "descrition": ""
+    }, {
+        "title": "Joker",
+        "director": "Tod Phillips",
+        "genre": "Thriller",
+        "year": "2019",
+        "price": "5.99",
+        "quantity": 95,
+        "cover": "FILES/COVER/tayfun-dereli-joker.jpg",
+        "poster": "FILES/POSTER/joker-2019-movie.jpg",
+        "descrition": ""
+    }, {
+        "title": "Jumanji:The Next Level",
+        "director": "Jake Kasdan",
+        "genre": "Fantasy",
+        "year": "2019",
+        "price": "0.99",
+        "quantity": 93,
+        "cover": "FILES/COVER/jumanji_the_next_level_ver2.jpg",
+        "poster": "FILES/POSTER/jumanji-the-next-level-kevin-hart-dwayne-johnson.jpg",
+        "descrition": ""
+    }, {
+        "title": "John Wick: Chapter 3 - Parabellum",
+        "director": "Chad Stahelski",
+        "genre": "Thriller",
+        "year": "2019",
+        "price": "1.99",
+        "quantity": 98,
+        "cover": "FILES/COVER/91kwZ-2DQ4L._SY679_.jpg",
+        "poster": "FILES/POSTER/movie-poster-of-john-wick-3.jpg",
+        "descrition": ""
+    }, {
+        "title": "The Dark Knight Rises",
+        "director": "Christopher Nolan",
+        "genre": "Thriller",
+        "year": "2012",
+        "price": "3.99",
+        "quantity": 99,
+        "cover": "FILES/COVER/51k98elC6mL._AC_.jpg",
+        "poster": "FILES/POSTER/movie-the-dark-knight-rises.jpg",
+        "descrition": ""
+    }, {
+        "title": "It Chapter Two",
+        "director": "Andres Muschietti",
+        "genre": "Horror",
+        "year": "2019",
+        "price": "1.99",
+        "quantity": 99,
+        "cover": "FILES/COVER/it-chapter-two-poster-2.jpg",
+        "poster": "FILES/POSTER/red-balloons-it-chapter-2-movie.jpg",
+        "descrition": ""
+    }]
+
+
+));
+
+localStorage.setItem("All_users", JSON.stringify([{
+        "name": "Igor",
+        "surname": "Kostin",
+        "password": "admin",
+        "admin": true,
+        "username": "Igor_Kostin",
+        "purchase": []
+    }, {
+        "name": "Marko",
+        "surname": "Markovic",
+        "password": "",
+        "admin": false,
+        "username": "Markovi_M1",
+        "purchase": []
+    }, {
+        "name": "Sinisa",
+        "surname": "Petrovic",
+        "password": "",
+        "admin": false,
+        "username": "SInisa2",
+        "purchase": []
+    }, {
+        "name": "Nikola",
+        "surname": "Gasic",
+        "password": "",
+        "admin": false,
+        "username": "Gasic+Nikola",
+        "purchase": []
+    }, {
+        "name": "John",
+        "surname": "Smith",
+        "password": "",
+        "admin": false,
+        "username": "MrJohn",
+        "purchase": []
+    }, {
+        "name": "Petar",
+        "surname": "Pecikoza",
+        "password": "",
+        "admin": true,
+        "username": "Pezikozaaa",
+        "purchase": []
+    }, {
+        "name": "Aleksa3",
+        "surname": "Kovacevic",
+        "password": "new",
+        "admin": false,
+        "username": "aleksaaa",
+        "purchase": []
+    }, {
+        "name": "Milana",
+        "surname": "Matic",
+        "password": "milana",
+        "admin": false,
+        "username": "MM",
+        "purchase": []
+    }, {
+        "name": "Svetlana",
+        "surname": "Nikolic",
+        "password": "1",
+        "admin": false,
+        "username": "svetlana",
+        "purchase": [0, 1, 2, 3, 4, 5, 6, 7]
+    }, {
+        "name": "Milica",
+        "surname": "Urosevic",
+        "password": "milica",
+        "admin": false,
+        "username": "milica",
+        "purchase": []
+    }, {
+        "name": "Milana",
+        "surname": "Peric",
+        "password": "milana",
+        "admin": false,
+        "username": "Milana023",
+        "purchase": []
+    }, {
+        "name": "Milorad",
+        "surname": "Miroavljevic",
+        "password": "milorad",
+        "admin": false,
+        "username": "Milorad",
+        "purchase": []
+    }, {
+        "name": "Nikola",
+        "surname": "Maljkovic",
+        "password": "nesto",
+        "admin": false,
+        "username": "Niidza",
+        "purchase": []
+    }, {
+        "name": "Igor",
+        "surname": "Kostin",
+        "password": "admin",
+        "admin": true,
+        "username": "Igor_Kostin2",
+        "purchase": []
+    }, {
+        "name": "Igor",
+        "surname": "kostin",
+        "password": "i",
+        "admin": false,
+        "username": "igor3",
+        "purchase": []
+    }, {
+        "name": "makro",
+        "surname": "nikolic",
+        "password": "m",
+        "admin": true,
+        "username": "Igor_Kostin4",
+        "purchase": []
+    }]
+
+
+));
+
+localStorage.setItem("Site_rating", JSON.stringify({
+        "value": "4.01",
+        "all_value": 569,
+        "count": 143
+    }
+
+));
+localStorage.setItem("All_director_in", JSON.stringify(["Rian Johnson", "Martin Scorsese", "Ryan Coogler", "James Wan", "Jon Faverea", "Anthony Russo", "James Bobin", "David Leitch", "Tod Phillips", "Jake Kasdan", "Chad Stahelski", "Christopher Nolan", "Andres Muschietti"]
+
+));
+
+localStorage.setItem("All_genres_in", JSON.stringify(["Drama", "Crime", "Fantasy", "Romance", "Sci-Fi", "Adventure", "Action", "Thriller", "Horror"]
+
+));
+
+localStorage.setItem("Global_purchuase", JSON.stringify([
+        [{
+            "title": "A Star Is Bortn",
+            "director": "Bradley Cooper",
+            "genre": "Romance",
+            "year": "2018",
+            "price": "3.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/a star is born.jpg"
+        }],
+        [{
+            "title": "Black Panther",
+            "director": "Rian Johnson",
+            "genre": "Fantasy",
+            "year": "2018",
+            "price": "1.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/black-panther3.jpg"
+        }],
+        [{
+            "title": "A Star Is Bortn",
+            "director": "Bradley Cooper",
+            "genre": "Romance",
+            "year": "2018",
+            "price": "3.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/a star is born.jpg"
+        }],
+        [{
+            "title": "Black Panther",
+            "director": "Rian Johnson",
+            "genre": "Fantasy",
+            "year": "2018",
+            "price": "1.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/black-panther3.jpg"
+        }, {
+            "title": "A Star Is Bortn",
+            "director": "Bradley Cooper",
+            "genre": "Romance",
+            "year": "2018",
+            "price": "3.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/a star is born.jpg"
+        }, {
+            "title": "Aquaman",
+            "director": "James Wan",
+            "genre": "Sci-Fi",
+            "year": "2018",
+            "price": "0.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/6319745_so.jpg"
+        }],
+        [{
+            "title": "Knivs Out",
+            "director": "Rian Johnson",
+            "genre": "Drama",
+            "year": "2019",
+            "price": "2.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/knivs_out.jpg"
+        }],
+        [{
+            "title": "John Wick: Chapter 3 - Parabellum",
+            "director": "Chad Stahelski",
+            "genre": "Thriller",
+            "year": "2019",
+            "price": "1.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/91kwZ-2DQ4L._SY679_.jpg"
+        }],
+        [{
+            "title": "Avengers Endgame",
+            "director": "Anthony Russo",
+            "genre": "Fantasy",
+            "year": "2019",
+            "price": "4.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/81%2BNup8-8NL._SL1500_.jpg"
+        }],
+        [{
+            "title": "Lion King",
+            "director": "Jon Faverea",
+            "genre": "Drama",
+            "year": "2019",
+            "price": "2.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/lion_king_ver22.jpg"
+        }, {
+            "title": "Avengers Endgame",
+            "director": "Anthony Russo",
+            "genre": "Fantasy",
+            "year": "2019",
+            "price": "4.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/81%2BNup8-8NL._SL1500_.jpg"
+        }, {
+            "title": "Fast & Furious Present: Hobbs & Shaw",
+            "director": "David Leitch",
+            "genre": "Action",
+            "year": "2019",
+            "price": "1.99",
+            "quantity": 2,
+            "cover": "FILES/COVER/2cb0b40827c1d8b53df0d68013674709.jpg"
+        }, {
+            "title": "Aquaman",
+            "director": "James Wan",
+            "genre": "Sci-Fi",
+            "year": "2018",
+            "price": "0.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/6319745_so.jpg"
+        }, {
+            "title": "Jumanji:The Next Level",
+            "director": "Jake Kasdan",
+            "genre": "Fantasy",
+            "year": "2019",
+            "price": "0.99",
+            "quantity": 3,
+            "cover": "FILES/COVER/jumanji_the_next_level_ver2.jpg"
+        }, {
+            "title": "A Star Is Bortn",
+            "director": "Bradley Cooper",
+            "genre": "Romance",
+            "year": "2018",
+            "price": "3.99",
+            "quantity": 1,
+            "cover": "FILES/COVER/a star is born.jpg"
+        }]
+    ]
+
+));
